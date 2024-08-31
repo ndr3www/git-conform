93d2fba07f277f6b8880d6ca4281e71013f447ea
ndr3www <ndr3.www.3@gmail.com>
2024-08-31 20:16:44 +0200

    core: implement recursive search for git repositories

--- src/core.rs
+++ src/core.rs
@@ -3,9 +3,15 @@
 use crate::utils::APP_NAME;
 
 use std::path::Path;
+use std::process::{Command, Stdio};
+
+use walkdir::WalkDir;
 
 /// Searches recursively for untracked git repositories and automatically adds them to the tracking file
+#[allow(clippy::redundant_closure_for_method_calls)]
 pub fn scan(dirs: &[String], all: bool) -> Result<(), String> {
+    // Directories validation
+
     let mut dirs_ok = true;
 
     for dir in dirs {
@@ -34,6 +40,32 @@ pub fn scan(dirs: &[String], all: bool) -> Result<(), String> {
 
     if !dirs_ok {
         return Err(String::from("Scan failed"));
+    }
+
+    // Scan the directories
+    for dir in dirs {
+        for entry in WalkDir::new(dir)
+                .follow_links(true)
+                .into_iter()
+                .filter_map(|n| n.ok()) {
+            // Check if the path contains .git directory
+            if let Some(path) = entry.path().to_str() {
+                if let Some(path_parent) = path.strip_suffix("/.git") {
+                    // Check if the path is in fact a git repository
+
+                    let git_status = Command::new("git")
+                        .args(["-C", path_parent, "status"])
+                        .stdout(Stdio::null())
+                        .stderr(Stdio::null())
+                        .status()
+                        .map_err(|e| format!("{e}"))?;
+
+                    if git_status.success() {
+                        // TODO: add git repository to the tracking file
+                    }
+                }
+            }
+        }
     }
 
     Ok(())
