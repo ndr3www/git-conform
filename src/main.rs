5aadc2e38bcfdefdbe99e821c34a91e02d084a18
ndr3www <ndr3.www.3@gmail.com>
2024-09-02 16:31:47 +0200

    main: sync the tracking file on startup

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ use crate::utils::{
 };
 use crate::cli::{Cli, Commands};
 
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::Path;
 use std::ptr::addr_of;
+use std::process::{Command, Stdio};
 
 use clap::Parser;
 
@@ -30,8 +33,40 @@ fn main() {
                 APP_DATA_DIR = format!("{HOME_DIR}/.local/share/{APP_NAME}");
                 APP_TRACK_FILE_PATH = format!("{APP_DATA_DIR}/tracked");
 
-                if let Ok(s) = fs::read_to_string(addr_of!(APP_TRACK_FILE_PATH).as_ref().unwrap()) {
-                    APP_TRACK_FILE = s;
+                if let Ok(str) = fs::read_to_string(addr_of!(APP_TRACK_FILE_PATH).as_ref().unwrap()) {
+                    APP_TRACK_FILE.clone_from(&str);
+
+                    // Check if the tracking file is up-to-date and remove obsolete entries if not
+
+                    for line in str.lines() {
+                        if Path::new(format!("{line}/.git").as_str()).exists() {
+                            if let Ok(git_status) = Command::new("git")
+                                .args(["-C", line, "status"])
+                                .stdout(Stdio::null())
+                                .stderr(Stdio::null())
+                                .status() {
+                                if !git_status.success() {
+                                    APP_TRACK_FILE = str.replace(line, "");
+                                }
+                            }
+                            else {
+                                handle_error(format!("{line}: Could not execute git command").as_str(), 1);
+                            }
+                        }
+                        else {
+                            APP_TRACK_FILE = str.replace(line, "");
+                        }
+                    }
+
+                    let mut track_file = File::create(
+                        addr_of!(APP_TRACK_FILE_PATH)
+                            .as_ref()
+                            .unwrap())
+                        .unwrap();
+                    match track_file.write_all(APP_TRACK_FILE.as_bytes()) {
+                        Ok(()) => (),
+                        Err(e) => handle_error(format!("{APP_TRACK_FILE_PATH}: {e}").as_str(), 1)
+                    }
                 }
             }
         }
