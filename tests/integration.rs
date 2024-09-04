03bec920e63c0b89dc1bc91fa09bca498450e141
ndr3www <ndr3.www.3@gmail.com>
2024-09-04 21:29:42 +0200

    tests: add test for add command

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,6 @@
 mod common;
 
+use git_conform::core::add;
 use git_conform::utils::search_for_repos;
 
 use std::fs;
@@ -16,6 +17,25 @@ fn case_scan() {
 
     // The function executes without errors
     assert_eq!(search_for_repos(&[project_root.to_string()], track_file_path.as_str(), track_file_contents.as_str()), Ok(()));
+
+    // Read the updated tracking file
+    let track_file_up = fs::read_to_string(track_file_path).unwrap();
+
+    // The tracking file contains the project's repository path
+    assert!(track_file_up.contains(project_root));
+}
+
+#[test]
+fn case_add() {
+    let essentials = common::setup().unwrap();
+    let track_file_path = &essentials[1];
+    let track_file_contents = &essentials[2];
+
+    let project_root_binding = env::current_dir().unwrap();
+    let project_root = project_root_binding.to_str().unwrap();
+
+    // The function executes without errors
+    assert_eq!(add(&[project_root.to_string()], track_file_path.as_str(), track_file_contents.as_str()), Ok(()));
 
     // Read the updated tracking file
     let track_file_up = fs::read_to_string(track_file_path).unwrap();
