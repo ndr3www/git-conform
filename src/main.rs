01b88a0b7755054081347ac1c3f2cef87a542d97
ndr3www <ndr3.www.3@gmail.com>
2024-09-07 00:04:15 +0200

    main: small comment correction

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn main() {
                     };
                 }
 
-                // Write final the changes to the tracking file
+                // Write the final the changes to the tracking file
                 match File::create(&track_file_path) {
                     Ok(mut f) => {
                         match f.write_all(track_file_contents.as_bytes()) {
