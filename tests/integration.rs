5f25ae0a2e453adcf34dafaa2141ff905c66f227
ndr3www <ndr3.www.3@gmail.com>
2024-09-06 23:57:52 +0200

    tests: add test for the rm command

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,9 +1,10 @@
 mod common;
 
-use git_conform::core::add;
+use git_conform::core::{add, remove_repos};
 use git_conform::utils::search_for_repos;
 
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
 use std::path::Path;
 use std::env;
 
@@ -55,4 +56,29 @@ fn case_add() {
 
     // The tracking file contains the project's repository path
     assert!(track_file_up.contains(project_root));
+}
+
+#[test]
+#[serial]
+fn case_remove_repos() {
+    let essentials = common::setup().unwrap();
+    let track_file_path = &essentials[1];
+
+    #[allow(clippy::items_after_statements)]
+    const TRACK_FILE_CONTENTS: &str = "repo1\nrepo2\nrepo3";
+
+    // Create/overwrite the tracking file
+    File::create(track_file_path)
+        .unwrap()
+        .write_all(TRACK_FILE_CONTENTS.as_ref())
+        .unwrap();
+
+    // The function executes without errors
+    assert_eq!(remove_repos(vec!["repo2".to_string()], track_file_path.as_str(), TRACK_FILE_CONTENTS), Ok(()));
+
+    // Read the updated tracking file
+    let track_file_up = fs::read_to_string(track_file_path).unwrap();
+
+    // The tracking file doesn't contain repo2
+    assert!(!track_file_up.contains("repo2"));
 }
