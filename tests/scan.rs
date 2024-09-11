5c253a56317065f10a4012e87d5bc6f2ea2620bc
ndr3www <ndr3.www.3@gmail.com>
2024-09-11 23:37:43 +0200

    tests: add failing tests to scan

--- tests/scan.rs
+++ tests/scan.rs
@@ -143,3 +143,33 @@ fn case_scan_all() {
         ));
     }
 }
+
+#[test]
+#[serial]
+fn case_scan_dirs_non_existent() {
+    let essentials = common::setup().unwrap();
+    let track_file_path = &essentials[1];
+
+    // The function throws an error
+    let dirs = vec![
+        format!("quegq4tq4q"),
+        format!("lvdslns"),
+        format!("fjioadbaob")
+    ];
+    assert_eq!(scan_dirs(dirs, track_file_path, "", true), Err(String::from("Directories validation failed")));
+}
+
+#[test]
+#[serial]
+fn case_scan_dirs_files() {
+    let essentials = common::setup().unwrap();
+    let track_file_path = &essentials[1];
+    let tests_dir = &essentials[2];
+    
+    // The function throws an error
+    let mut dirs: Vec<String> = Vec::new();
+    for n in 1..=3 {
+        dirs.push(format!("{tests_dir}/file{n}"));
+    }
+    assert_eq!(scan_dirs(dirs, track_file_path, "", true), Err(String::from("Directories validation failed")));
+}
