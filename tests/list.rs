70c73de02ea66224d5d668090a5890c2679d4b59
ndr3www <ndr3.www.3@gmail.com>
2024-10-01 15:23:05 +0200

    tests: remove unnecessary setup from case_list_empty

--- tests/list.rs
+++ tests/list.rs
@@ -1,11 +1,7 @@
-mod common;
-
 use git_conform::core::list;
 
 #[test]
 fn case_list_empty() {
-    common::setup().unwrap();
-
     // The function throws an error
     assert_eq!(list(""), Err(String::from("No repository is being tracked")));
 }
